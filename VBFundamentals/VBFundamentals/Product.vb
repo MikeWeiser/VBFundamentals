78b33850777bea1022377d5a45aa75f1074f8c4d
Michael Weiser <Michael.Weiser@mychoicewi.org>
2022-12-05 16:10:22 -0600

    Mod 3, end of Create Product Class

---
+++ VBFundamentals/VBFundamentals/Product.vb
@@ -0,0 +1,34 @@
+﻿Public Class Product
+    Private _IsActive As Boolean
+    Public Property IsActive() As Boolean
+        Get
+            Return _IsActive
+        End Get
+        Set(ByVal value As Boolean)
+            _IsActive = value
+        End Set
+    End Property
+
+    Private _Name As String
+    Public Property Name() As String
+        Get
+            Return _Name
+        End Get
+        Set(ByVal value As String)
+            _Name = value
+        End Set
+    End Property
+
+    Private _ProductNumber As String
+    Public Property ProductNumber() As String
+        Get
+            Return _ProductNumber
+        End Get
+        Set(ByVal value As String)
+            _ProductNumber = value
+        End Set
+    End Property
+
+
+End Class
+
