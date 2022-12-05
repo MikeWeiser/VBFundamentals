bac8066881e529ee00978d8c221c11b21bf45080
Michael Weiser <Michael.Weiser@mychoicewi.org>
2022-12-05 17:09:33 -0600

    Mod 4, End of AutoImplemented Properties

--- VBFundamentals/VBFundamentals/Product.vb
+++ VBFundamentals/VBFundamentals/Product.vb
@@ -1,34 +1,15 @@
 ﻿Public Class Product
-    Private _IsActive As Boolean
-    Public Property IsActive() As Boolean
-        Get
-            Return _IsActive
-        End Get
-        Set(ByVal value As Boolean)
-            _IsActive = value
-        End Set
-    End Property
+    Public Property IsActive As Boolean
 
-    Private _Name As String
-    Public Property Name() As String
-        Get
-            Return _Name
-        End Get
-        Set(ByVal value As String)
-            _Name = value
-        End Set
-    End Property
+    Public Property Name As String
 
-    Private _ProductNumber As String
-    Public Property ProductNumber() As String
-        Get
-            Return _ProductNumber
-        End Get
-        Set(ByVal value As String)
-            _ProductNumber = value
-        End Set
-    End Property
+    Public Property ProductNumber As String
 
+    Public ReadOnly Property NameAndNumber() As String
+        Get
+            Return Name + "-" + ProductNumber
+        End Get
+    End Property
 
 End Class
 
