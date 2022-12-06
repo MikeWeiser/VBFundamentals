87b079cdcda74b0643cd5b8b6aa382cf44d5c18b
Michael Weiser <Michael.Weiser@mychoicewi.org>
2022-12-05 18:28:00 -0600

    Mod 4, End of Pass Parameters by Ref

--- VBFundamentals/VBFundamentals/MainModule.vb
+++ VBFundamentals/VBFundamentals/MainModule.vb
@@ -3,6 +3,13 @@ Imports System
 Module MainModule
     Sub Main()
 
+        Dim prod As New Product
+        Dim sellDate As DateTime
+
+        prod.SellStartDate = #5/1/2019#
+        prod.CalculateSellEndDate(30, sellDate) 'adds 20 days to start date and assigns to SellEndDate
+
+        Console.WriteLine(sellDate)
 
         Console.ReadKey()
     End Sub
--- VBFundamentals/VBFundamentals/Product.vb
+++ VBFundamentals/VBFundamentals/Product.vb
@@ -5,11 +5,23 @@
 
     Public Property ProductNumber As String
 
-    Public ReadOnly Property NameAndNumber() As String
-        Get
-            Return Name + "-" + ProductNumber
-        End Get
-    End Property
+    Public Property StandardCost As Decimal
+
+    Public Property ListPrice As Decimal
+
+    Public Property Size As String
+
+    Public Property Weight As Decimal
+
+    Public Property SellStartDate As DateTime
+
+    Public Property SellEndDate As DateTime
+
+    Sub CalculateSellEndDate(ByVal days As Integer, ByRef sellDate As DateTime)
+        SellEndDate = SellStartDate.AddDays(days)
+
+        sellDate = SellEndDate
+    End Sub
 
 End Class
 
